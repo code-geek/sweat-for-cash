6a684400d3fb3d53b3d40db764176c26c6ef883d
Ashish Acharya <ashish.acharya14@gmail.com>
2024-04-27 08:24:36 -0500

    Add models for Payment and Transaction

--- pools/models.py
+++ pools/models.py
@@ -10,3 +10,21 @@ class Pool(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Transaction(models.Model):
+    pool = models.ForeignKey(
+        Pool,
+        on_delete=models.CASCADE,
+        related_name="transactions",
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="transactions_made",
+    )
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} {self.amount}"
