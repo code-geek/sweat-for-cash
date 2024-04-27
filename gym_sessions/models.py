6f38a3a0deffc9d8c935327208fcc783b7a022a8
Ashish Acharya <ashish.acharya14@gmail.com>
2024-04-27 08:12:40 -0500

    Add models for GymSession

--- gym_sessions/models.py
+++ gym_sessions/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class GymSession(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    note = models.TextField(blank=True, default="")
+
+    def __str__(self):
+        return (
+            f"Session on {self.date.strftime('%Y-%m-%d %H:%M')} by {self.user.username}"
+        )
+
+
+class GymSessionImage(models.Model):
+    session = models.ForeignKey(
+        GymSession,
+        related_name="images",
+        on_delete=models.CASCADE,
+    )
+    image = models.ImageField(upload_to="gym_sessions/")
+    upload_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Image for session on {self.session.date.strftime('%Y-%m-%d %H:%M')} uploaded on {self.upload_date.strftime('%Y-%m-%d %H:%M')}"  # noqa: E501
