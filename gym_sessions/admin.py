75b7b4221a1309827bf00d637c817ebe94e1092c
Ashish Acharya <ashish.acharya14@gmail.com>
2024-04-27 08:31:43 -0500

    Add admin entries for all models

--- gym_sessions/admin.py
+++ gym_sessions/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import GymSession
+from .models import GymSessionImage
+
+
+@admin.register(GymSession)
+class GymSessionAdmin(admin.ModelAdmin):
+    list_display = ("user", "date", "note")
+
+
+@admin.register(GymSessionImage)
+class GymSessionImageAdmin(admin.ModelAdmin):
+    """Admin View for GymSessionImage"""
+
+    list_display = ("session", "image", "upload_date")
--- pools/admin.py
+++ pools/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Pool
+from .models import Transaction
+
+
+@admin.register(Pool)
+class PoolAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(Transaction)
+class Transaction(admin.ModelAdmin):
+    list_display = ("pool", "user", "amount", "created_at")
