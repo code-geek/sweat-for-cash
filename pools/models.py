a65b93345a4a399d8aa3c46b1b857964739dab70
Ashish Acharya <ashish.acharya14@gmail.com>
2024-04-27 08:16:52 -0500

    Add Pool model

--- pools/models.py
+++ pools/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Pool(models.Model):
+    name = models.CharField(max_length=255)
+    participants = models.ManyToManyField(User, related_name="pools")
+
+    def __str__(self):
+        return self.name
